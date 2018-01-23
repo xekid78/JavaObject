a75d9d8ed7175773b775bb124e6c8e58d68ffdd2
Makoto Tanabe <xekid78@outlook.com>
2018-01-23 20:07:45 +0900

    文字列も配列もオブジェクト

---
+++ Sample28.java
@@ -0,0 +1,24 @@
+/**
+ *
+ * @author xekid78
+ *
+ */
+public class Sample28 {
+
+	public static void main(String[] args) {
+		// 文字列
+		String text = new String("あいうえお");
+		System.out.println(text);
+		System.out.println(text.length());
+
+		// 配列
+		int numbers[] = new int[5];
+		for (int num : numbers) {
+			System.out.print(num + ",");
+		}
+		System.out.println("");
+		System.out.println(numbers.length);
+
+	}
+
+}
